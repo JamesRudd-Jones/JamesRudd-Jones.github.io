d7a0506ef109350f2ddb5268df39cc7f1ba54bf8
james_dell <james.rudd.jones@gmail.com>
2026-06-15 14:23:50 +0100

    added some musings

--- src/assets/writings/dubstep_lack/writing_page-dubstep_lack.js
+++ src/assets/writings/dubstep_lack/writing_page-dubstep_lack.js
@@ -24,6 +24,17 @@ const WritingPageDubstepLack = () => {
         some circling of the lack that lacan mentions, some subconscious sublimation perhaps?
         always struggled to place my interest in large spaces and sounds, they emulate some sadness perhaps?
         is it some manic defence too perhaps, noise and motion in this larger empty void?
+
+        real digression - perhaps christain religion sees the first sin/transgession by adam and eve as this answer to the lack we all feel?
+        feel like must have been written about before
+        but it's like an answer to why we feel this lack, we had the garden of eden and we had it all, and instead we can place our blame on adam and eve for this lack that we feel
+        humans forever are trying to fill in and recover from this heavan on earth? will think more about it
+
+        quick google has given the following:
+        https://www.qeios.com/read/6V7ACV
+        https://deborahjbrasket.com/2014/02/28/thou-art-that-part-iv-some-tragic-falling-off/
+        https://medium.com/@marko.kostic97/primordial-guilt-from-the-mirror-to-the-fall-99b6cef32f94
+        https://www.gutenberg.org/files/8525/8525-h/8525-h.htm
       </FullWidth>
 
     </div>
